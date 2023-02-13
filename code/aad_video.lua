8d334a52001811942ebf7e1ae06c09d0a45a2765
dkienenb <dkienenb@gmail.com>
2023-02-13 12:30:04 -0500

    Complete intro subsystem

--- code/aad_video.lua
+++ code/aad_video.lua
@@ -16,14 +16,14 @@ function make_video_system(name, init_function, frame_function, ticks_per_frame,
       current_system = next_system
     end
     if (tick % ticks_per_frame) == 0 then
-      video_frame = video_frame + 1
       if (video_frame == frame_count) then
         current_system = next_system
       else
+        video_frame = video_frame + 1
         frame_function(video_frame)
       end
       if video_frame == 1 then
-        print_centered("(Press X to skip)", 200, 120)
+        print_centered("(Press X to skip)", 195, 125)
       end
     end
     tick = tick + 1
---
+++ code/intro.lua
@@ -0,0 +1,37 @@
+---
+--- Created by dkienenb.
+--- DateTime: 2/13/23 12:00 PM
+--- Intro subsystem
+---
+
+function intro_init()
+
+end
+
+function intro_init()
+  intro_frames = {
+    {
+      text="Lab46",
+      color=3
+    },
+    {
+      text="CCC",
+      color=1
+    },
+    {
+      text="Crew46 Presents",
+      color=12
+    }
+  }
+end
+
+function intro_frame(frame)
+  cls(13)
+  local display = intro_frames[frame]
+  print_centered(display.text, 240/2, 136/2, display.color)
+end
+
+intro_init()
+
+make_video_system("intro", intro_init, intro_frame, 60*3, #intro_frames, "splash")
+-- end intro
