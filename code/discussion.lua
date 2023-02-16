7f0065cfe3e5cda851f1c3d160908568110a34bc
dkienenb <dkienenb@gmail.com>
2023-02-16 11:31:11 -0500

    prototype discussion system

--- code/discussion.lua
+++ code/discussion.lua
@@ -19,12 +19,42 @@ function discussion_init()
           :add_response("Example response 3", 3, 3, 3, 3)
           :add_response("Example response 4", 4, 4, 4, 4)
   -- todo remove debugging statements
-  player={eloquence=2, charisma=2}
+  player={ingenuity=2, charisma=2, acuity=5}
+
+  function discussion_logic_loop()
+    local number_of_questions = #questions
+    if not selected_question then
+      local question_index = math.random(number_of_questions)
+      selected_question = table.remove(questions, question_index)
+    end
+    if not selected_question.selected_responses then
+      local responses = selected_question.responses
+      local selected_responses = {}
+      for _ = 1, player.ingenuity do
+        local response_index = math.random(#responses)
+        local response = table.remove(responses, response_index)
+        table.insert(selected_responses, response)
+      end
+      selected_question.selected_responses = selected_responses;
+    end
+  end
+
+  function print_question(question)
+    print_centered(question.question_text, 120, 20, 11)
+    for i = 1, #question.selected_responses do
+      print_centered(question.selected_responses[i].response_text, 120, 20 + (10 * i), 13)
+    end
+  end
+
+  function discussion_graphics_loop()
+    cls()
+    print_question(selected_question)
+  end
 end
 
 function discussion_loop()
-  -- todo implement this
-  cls(3)
+  discussion_logic_loop()
+  discussion_graphics_loop()
 end
 
 make_system("discussion", discussion_init, discussion_loop)
