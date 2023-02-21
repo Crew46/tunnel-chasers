e98669e85a74dad33eddbdd4b56b8b3bfdcf4e28
dkienenb <dkienenb@gmail.com>
2023-02-21 11:00:50 -0500

    ignore more files, update graphics

--- code/aab_graphics.lua
+++ code/aab_graphics.lua
@@ -10,13 +10,19 @@ function draw(thing, variant, x, y, scale)
   local rotate = 0
   local width = 1
   local height = 1
-  if thing == "player_portrait" then
-    local portraits = {8, 8, 0, 8, 226}
-    sprite_number = portraits[variant]
-    width = 2
-    height = 2
+  if thing then
+    if thing == "player_portrait" then
+      local portraits = {8, 8, 0, 8, 226}
+      sprite_number = portraits[variant]
+      width = 2
+      height = 2
+    elseif thing == "player_portrait_box" then
+      rectb(x, y, 32, 32, 11)
+    end
   end
-  spr(sprite_number, x, y, color_key, scale or 1, flip, rotate, width, height)
+  if sprite_number then
+    spr(sprite_number, x, y, color_key, scale or 1, flip, rotate, width, height)
+  end
 end
 
 -- end graphics
