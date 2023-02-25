f7c78e02ca0c51cbe67fd37ddb80e28fc1accc6f
Ash <101850594+AshBC91@users.noreply.github.com>
2023-02-25 18:46:05 -0500

    add updated graphics

--- code/aab_graphics.lua
+++ code/aab_graphics.lua
@@ -3,20 +3,20 @@
 --- DateTime: 2/21/23 9:56 AM
 --- graphics library
 
-function draw(thing, variant, x, y, scale)
+function draw(sprite_name, sprite_variant, x, y, scale)
   local sprite_number
   local color_key = -1
   local flip = 0
   local rotate = 0
   local width = 1
   local height = 1
-  if thing then
-    if thing == "player_portrait" then
+  if sprite_name then
+    if sprite_name == "player_portrait" then
       local portraits = {8, 8, 0, 8, 226}
-      sprite_number = portraits[variant]
+      sprite_number = portraits[sprite_variant]
       width = 2
       height = 2
-    elseif thing == "player_portrait_box" then
+    elseif sprite_name == "player_portrait_box" then
       rectb(x, y, 32, 32, 11)
     end
   end
