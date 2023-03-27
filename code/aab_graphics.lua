a5456cbb857868d3dd52ef1e6111d00acc627066
Ash <101850594+AshBC91@users.noreply.github.com>
2023-03-27 19:39:05 -0400

    add updated graphics lib code:

--- code/aab_graphics.lua
+++ code/aab_graphics.lua
@@ -1,50 +1,111 @@
 ---
---- Created by dkienenb.
---- DateTime: 2/21/23 9:56 AM
+--- Created by AshBC.
+--- DateTime: 03/25/23 10:00 AM
 --- graphics library
-pc_spr={
-  --character sprites are located in the sprite sheet of bank 1
-  --the first sprite is located on 256
-  --the last sprite is located on 511
-  --each character has up to 64 8x8 sprites
-  --left sprites are even, two spaces away from each other
-  --right sprites are odd, two spaces away from each other
-  --the character is facing forward
-  dpig={
 
+fr=0
+--bttn={u=0,d=1,l=2,r=3,z=4,x=5,a=6,s=7}
+bttn={u=0,d=1,l=2,r=3,w=23,s=19,a=1,d=4,q=17,e=5,z=26,x=24}
 
-    headfl_a=256,
-    headfr_a=257,
-    headfl_b=258,
-    headfr_b=259,
-    headbl_a=260,
-    headbr_a=261,
-    headbl_b=262,
-    headbr_b=263,
-
-  }
+pc={x=240/2,y=136/2,sprId=256,CLRK=0,scale=1,flip=0,
+	indx=1,changeFrame=false,CF=1,
+	wait=30,isIdle=true,isRun=false,
+	name={"pig","nul","par","byz"},selected="pig",
+ iniSpr={256,288,320,352},
+	sprites={}
 }
 
-function draw(sprite_name, sprite_variant, x, y, scale)
-  local sprite_number
-  local color_key = -1
-  local flip = 0
-  local rotate = 0
-  local width = 1
-  local height = 1
-  if sprite_name then
-    if sprite_name == "player_portrait" then
-      local portraits = {8, 8, 0, 8, 226}
-      sprite_number = portraits[sprite_variant]
-      width = 2
-      height = 2
-    elseif sprite_name == "player_portrait_box" then
-      rectb(x, y, 32, 32, 11)
-    end
-  end
-  if sprite_number then
-    spr(sprite_number, x, y, color_key, scale or 1, flip, rotate, width, height)
-  end
+for n=1,4 do
+	local spr_id=pc.iniSpr[n]
+	pc.sprites[n]={}
+	for c=1,8 do
+		pc.sprites[n][c]=spr_id
+		spr_id=spr_id+2
+	end
+end
+
+function pcSpr()
+	if keyp(bttn.q,60,15) then 
+		pc.indx=(pc.indx>1) and pc.indx-1 or 1
+		pc.selected=pc.name[pc.indx]
+		pc.sprId=pc.iniSpr[pc.indx]
+	elseif keyp(bttn.e,60,15) then 
+		pc.indx=(pc.indx<=3)and pc.indx+1 or 4
+		pc.selected=pc.name[pc.indx]
+		pc.sprId=pc.iniSpr[pc.indx]
+	end
+	print(pc.sprId,0,6,12)
+	print("Selected char: "..pc.selected,0,12,12)
+end
+
+function resetPos()
+	if keyp(bttn.z,60,15) then
+		pc.x=70
+		pc.y=75
+	end
+end
+
+function mvChar()
+	local hold=3
+ local period=1
+	if keyp(bttn.w,hold,period) then
+		pc.y=pc.y-.5
+	elseif keyp(bttn.s,hold,period) then
+		pc.y=pc.y+.5
+	else 
+		pc.isIdle=true
+		pc.isRun=false
+	end
+	if keyp(bttn.a,hold,period) then 
+		pc.x=pc.x-.5
+		pc.flip=1
+	elseif keyp(bttn.d,hold,period) then 
+		pc.x=pc.x+.5
+		pc.flip=0
+	else 
+		pc.isIdle=true
+		pc.isRun=false
+	end
+	if keyp(bttn.w,hold,period) or keyp(bttn.s,hold,period) or keyp(bttn.a,hold,period) or keyp(bttn.d,hold,period) then
+		pc.isIdle=false
+		pc.isRun=true
+		pc.changeFrame=true
+	end
+	--if keyp(bttn.x,60,6) then
+		--local iniy=pc.y
+        --pc.y=
+	--end
+end
+
+function animate()
+	print("Change frame: "..pc.CF,0,18,12)
+	if pc.wait == 0 then
+		pc.changeFrame=true
+		pc.CF=(pc.CF==1) and 2 or 1
+		if pc.isIdle then pc.wait=30
+		elseif pc.isRun then pc.wait=15
+		end
+	end
+	if pc.isIdle and pc.changeFrame then
+		pc.sprId=pc.sprites[pc.indx][pc.CF]
+		pc.changeFrame=false
+	elseif pc.isRun and pc.changeFrame then
+		pc.sprId=pc.sprites[pc.indx][pc.CF+2]
+		pc.changeFrame=false
+	end
+end
+
+function draw()
+	
+	pcSpr()
+	mvChar()
+	resetPos()
+	animate()
+	spr(pc.sprId,pc.x,pc.y,pc.CLRK,pc.scale,pc.flip,0,2,2)
+	print("Frame: "..fr,0,130,12)
+	fr=(fr+1)%60
+	pc.wait=pc.wait-1
+		
 end
 
 -- end graphics
