15d25e12aafc062d9c506f37cbc411d9a6d388fa
SockNastre <39687969+SockNastre@users.noreply.github.com>
2023-04-28 21:27:26 -0400

    Made new  to work with collisionbox

--- code/aab_graphics.lua
+++ code/aab_graphics.lua
@@ -5,16 +5,16 @@
 
 fr=0
 --bttn={u=0,d=1,l=2,r=3,z=4,x=5,a=6,s=7}
-bttn={u=0,d=1,l=2,r=3,w=23,s=19,a=1,d=4,q=17,e=5,z=26,x=24,shift=64}
+--bttn={u=0,d=1,l=2,r=3,w=23,s=19,a=1,d=4,q=17,e=5,z=26,x=24,shift=64}
 
 function pcSpr_change()
-	if keyp(bttn.q,60,15) then 
+	if keyp(17,60,15) then 
 		pc.indx=(pc.indx>1) and pc.indx-1 or 1
 		pc.selected=pc.nameTbl[pc.indx]
 		pc.spr_Id_h=pc.sprTbl[pc.indx]
 		pc.spr_Id_b=pc.spr_Id_h+8
 		pc.speed=pc.spdTbl[pc.indx]
-	elseif keyp(bttn.e,60,15) then 
+	elseif keyp(5,60,15) then 
 		pc.indx=(pc.indx<=4)and pc.indx+1 or 5
   		pc.selected=pc.nameTbl[pc.indx]
 		pc.spr_Id_h=pc.sprTbl[pc.indx]
@@ -27,73 +27,101 @@ function pcSpr_change()
 end
 
 function resetPos()
-	if keyp(bttn.z,60,15) then
+	if keyp(26,60,15) then
 		pc.x=70
 		pc.y=75
 	end
 end
 
 function pcActions()
-	local hold=1
- 	local diag_rate=0.78
-	local rate=pc.speed
-	local hor_spd=0
-	local ver_spd=0
-	local period=key(bttn.shift) and 2 or 1
-	
-	--Diagonal speed change
-	if ((key(bttn.w) or key(bttn.s)) 
-	and (key(bttn.a) or key(bttn.d))) then
-		rate=rate*diag_rate
+	-- functions local constants
+	local FLAG_COLLISION = 0
+	local rollover_amount = 4
+	local overlay_offset = {
+		x = 120 * 8,
+		y = 0
+	}
+
+	-- button press hold and period... period is 2 if player
+	-- is sneaking (pressing Shift key)
+	local hold   = 1
+	local period = key(64) and 2 or 1
+
+	-- rate of movement, and speeds of character
+	local rate    = pc.speed
+	local hor_spd = 0
+	local ver_spd = 0
+
+	-- if player is moving diagonally, apply diagonal rate
+	if ((key(23) or key(19))  and
+		(key(01) or key(04))) then
+		rate = rate * 0.78
 	end
-	
-	--Block unwanted input
-	if ((key(bttn.a) and key(bttn.d)) 
-		or (key(bttn.w) and key(bttn.s))
-		or ((key(bttn.a) and key(bttn.d)) and (key(bttn.w) or key(bttn.s)))
-		or ((key(bttn.w) and key(bttn.s)) and (key(bttn.a) or key(bttn.d))))
-		and key(bttn.shift) then 
-			hor_spd=0
-			ver_spd=0
-	else
-		--Horizontal speed
-		if keyp(bttn.a,hold,period) then 
-			pc.flip=1
-			hor_spd=hor_spd-rate
-		elseif keyp(bttn.d,hold,period) then 
-			pc.flip=0
-			hor_spd=hor_spd+rate
+
+	----------------------------------
+	-- Player pressed up button.    --
+	----------------------------------
+	if keyp(23, hold, period) then
+		-- points that may end up colliding
+		collisionPoints = collisionbox_flagchecks(FLAG_COLLISION, {0, 1}, {roofCheck = 1})
+
+		if #collisionPoints == 0 then
+			pc.isTurned = true
+			ver_spd = ver_spd - rate
 		end
-		
-		--Vertical speed
-		if keyp(bttn.w,hold,period) then
-			pc.isTurned=true
-			ver_spd=ver_spd-rate
-		elseif keyp(bttn.s,hold,period) then
-			pc.isTurned=false
-			ver_spd=ver_spd+rate
+	end
+	----------------------------------
+	-- Player pressed down button.  --
+	----------------------------------
+	if keyp(19, hold, period) then
+		-- points that may end up colliding
+		collisionPoints = collisionbox_flagchecks(FLAG_COLLISION, {2, 3}, {roofCheck = 1})
+
+		if #collisionPoints == 0 then
+			pc.isTurned = false
+			ver_spd = ver_spd + rate
+		end
+	end
+	----------------------------------
+	-- Player pressed left button.  --
+	----------------------------------
+	if keyp(01, hold, period) then
+		-- points that may end up colliding
+		collisionPoints = collisionbox_flagchecks(FLAG_COLLISION, {0, 2}, {wallCheck = 1})
+
+		if #collisionPoints == 0 then
+			pc.flip = 1
+			hor_spd = hor_spd - rate
+		end
+	end
+	----------------------------------
+	-- Player pressed right button. --
+	----------------------------------
+	if keyp(04, hold, period) then
+		-- points that may end up colliding
+		collisionPoints = collisionbox_flagchecks(FLAG_COLLISION, {1, 3}, {wallCheck = 1})
+
+		if #collisionPoints == 0 then
+			pc.flip = 0
+			hor_spd = hor_spd + rate
 		end
 	end
 
-	--Change player state
-	pc.isCrouch=(key(bttn.shift)) and true or false
+	pc.isCrouch = (key(64)) and true or false
 
-	if ((key(bttn.a) and key(bttn.d))
-	or (key(bttn.w) and key(bttn.s))) 
-	and key(bttn.shift) then
+	if ((key(01) and key(04)) or (key(23) and key(19))) 
+	and key(64) then
 		pc.isIdle=true
 		pc.isRun=false
 		pc.state="Idle"
 		pc.changeFrame=true
-	elseif (key(bttn.w) or key(bttn.s) 
-	or key(bttn.a) or key(bttn.d))
+	elseif (key(23) or key(19) or key(01) or key(04))
 	and isCrouch then
 		pc.isIdle=false
 		pc.isRun=false
 		pc.state="Crouched"
 		pc.changeFrame=true
-	elseif key(bttn.w) or key(bttn.s) 
-	or key(bttn.a) or key(bttn.d) then
+	elseif key(23) or key(19) or key(01) or key(04) then
 		pc.isIdle=false
 		pc.isRun=true
 		pc.state="Running"
@@ -105,15 +133,15 @@ function pcActions()
 		pc.changeFrame=true
 	end
 
-	pc.x=pc.x+hor_spd
-	pc.y=pc.y+ver_spd
-
+	pc.x = pc.x + hor_spd
+	pc.y = pc.y + ver_spd
 end
 
 function animate()
 	print("Change frame: "..pc.CF,0,18,6)
   	print("Change frame timer: "..pc.CF_timer,30,0,6)
 	print("PC state: "..pc.state,50,70,12)
+	trace(pc.CF_timer)
 	if pc.CF_timer == 0 then
 		pc.changeFrame=true
 		pc.CF=(pc.CF==1) and 2 or 1
@@ -203,6 +231,6 @@ function draw(sprite_name, sprite_variant, x, y, scale)
 	  spr(sprite_number_head, x, y, color_key, scale or 1, flip, rotate, width, height)
 	  spr(sprite_number_body, x, y+16, color_key, scale or 1, flip, rotate, width, height)
 	end
-  end
+end
 
 -- end graphics
