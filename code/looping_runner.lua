9e0303d6bab18a10fd5fe6be7b6ee6149ca1c140
Macklen <fmacklen4@gmail.com>
2023-04-29 15:46:29 -0400

    add updated looping_runner.lua

--- code/looping_runner.lua
+++ code/looping_runner.lua
@@ -2,167 +2,171 @@
 -- Created by: MacklenF, AshBC, Sashami 
 -- desc:   Looping Runner PrototypeV2
 -- script: lua
- 
+
 function looping_runner_init()
   --gsync(16,0,false)
   --music(1,0,0,true)
   fr=0
   bttn={u=0,d=1,l=2,r=3,z=4,x=5,a=6,s=7}
-  selected_pc="pig"
-
-  pc={x=55,y=78,sprId=256,CLRK=0,scale=2,flip=0,
-      indx=1,changeFrame=false,CF=1,
-      r_wait=30,isIdle=true,isRun=false,
-      name={"pig","nul","par","byz"},
-      iniSpr={256,288,320,352},
-      spdTbl={0.8,0.65,0.75,0.7},
-     sprites={}
-  }
-
-  for n=1,4 do
-   local spr_id=pc.iniSpr[n]
-     pc.sprites[n]={}
-     for c=1,8 do
-       pc.sprites[n][c]=spr_id
-       spr_id=spr_id+2
-     end
-  end
-  -- runner vars 
-  local gm_state = {win=false,fail=false}
   local tm = {gm_ct=30*60, wt=60, intvl=0} -- timers
+  local gm_state = {win=false,fail=false}
+  local obs = {} -- obstacles
+  local boosts  = {}
   local y_vel = 0
+  local mp_spd = -.3
   run_floor = 78 -- =player y pos
   jump_max = 60
   cur_dist = 6.0
-  grav = 0.2     -- speed for jump fall   
-  local obs = {} -- obstacles
-  local boosts  = {}
+  grav = 0.2     -- speed for jump fall
+  mapX = 0
+  decor = {x=0,y=34}  
+  -- officer table
+  ofc = { x=200,id=484,runId=488,idleId=480,hitId=492,
+         flip=1,FR=0,t=30 }
+		 
+  -- init rt_pc
+  selected_pc="pig"
 
-  ofc = {x=200,id=484,flip=1} --officer table
-  -- officer vars
-  o = {runId=488,idleId=480,hitId=492,FR=0,t=10}
-  mapX = 210 
-  decor = {x=210, y=34} 
-  
-  local p = pc
-  p.x = 152
-  p.isIdle = false
-  p.isRun=true
-  p.changeFrame=true
-  p.flip = 1
-  mp_spd = -.25
+  rt_pc={x=55,y=78,sprId=256,CLRK=0,scale=2,flip=0,
+	  indx=1,changeFrame=false,CF=1,
+	  wait=30,isIdle=true,isRun=false,
+	  name={"pig","nul","par","byz"},
+ 	  iniSpr={256,288,320,352},
+   spdTbl={0.8,0.65,0.75,0.7},
+	  sprites={}
+  }
 
-  function pcSpr()
-    if btnp(bttn.a,60,15) then 
-      pc.indx=(pc.indx>1) and pc.indx-1 or 1
-      selected_pc=pc.name[pc.indx]
-      pc.sprId=pc.iniSpr[pc.indx]
-    elseif btnp(bttn.s,60,15) then 
-      pc.indx=(pc.indx<=3)and pc.indx+1 or 4
-      selected_pc=pc.name[pc.indx]
-      pc.sprId=pc.iniSpr[pc.indx]
+  for n=1,4 do
+	  local spr_id=rt_pc.iniSpr[n]
+	  rt_pc.sprites[n]={}
+	  for c=1,8 do
+		  rt_pc.sprites[n][c]=spr_id
+		  spr_id=spr_id+2
+	  end
+  end
+  -- set player 
+  rt_pc.x = 152
+  rt_pc.isIdle = false
+  rt_pc.isRun=true
+  rt_pc.changeFrame=true
+  rt_pc.flip = 1   
+  -- set player speeds
+  for i=1,4 do
+  --  if pc.selected == rt_pc.name[i] then
+    if selected_pc == rt_pc.name[i] then
+      rt_pc.indx = i
+      sprSpd = rt_pc.spdTbl[i]
     end
   end
+  tm.gm_ct = tm.gm_ct/sprSpd -- set game time
 
-  function rt_animate()
-    if pc.r_wait == 0 then
-      pc.changeFrame=true
-      pc.CF=(pc.CF==1) and 2 or 1
-      if pc.isIdle then pc.r_wait=30
-      elseif pc.isRun then pc.r_wait=15
-      end
-    end
-    if pc.isIdle and pc.changeFrame then
-      pc.sprId=pc.sprites[pc.indx][pc.CF]
-      pc.changeFrame=false
-    elseif pc.isRun and pc.changeFrame then
-      pc.sprId=pc.sprites[pc.indx][pc.CF+4]
-      pc.changeFrame=false
-    end
-  end
-  
-  function rt_draw()
-    pcSpr()
-    rt_animate()
-    if (pc.y >= 78) then
-      spr(445,ofc.x+6,101,5,2,1,0,1,1)--shadow officer
-      spr(445,pc.x+8,pc.y+24,5,2,1,0,1,1)--shadow pc
-      spr(ofc.id+o.FR//20*2,ofc.x,78,0,2,ofc.flip,0,2,2)--oficcer
-      spr(pc.sprId,pc.x,pc.y,pc.CLRK,pc.scale,pc.flip,0,2,2)--pc
-    else
-      spr(445,pc.x+8,102,5,2,1,0,1,1)--shadow pc
-      spr(445,ofc.x+6,101,5,2,1,0,1,1)--shadow officer
-      spr(pc.sprId,pc.x,pc.y,pc.CLRK,pc.scale,pc.flip,0,2,2)--pc
-      spr(ofc.id+o.FR//20*2,ofc.x,78,0,2,ofc.flip,0,2,2)--officer
-    end
-  end
-
-  function print_debug(color)
-    print("Press A or S to change character",0,0,color or 3)	
-    print("Change frame: "..pc.CF,0,7,color or 12)
-    print(", ID: "..pc.sprId,83,7,color or 12)
-    print(", Character: "..selected_pc,126,7,color or 12)
-    print("UP to jump  DOWN to slide",0,14,color or 3)
-    print("player y: "..p.y,160,14,12)	
-  end
-
-  function runner_sprsheet03()
-    cls()
-    vbank(0)
-    map(mapX,17,30,17,0,0,-1)--bg
-    map(decor.x,decor.y,30,17,0,3,0,2)--decorations
-    vbank(1)
-    poke(0x03FF8,0)
-    map(mapX,17,30,17,0,0,-1)--bg
-    map(decor.x,decor.y,30,17,0,3,0,2)--decorations
-    rt_draw()
-
-    fr=(fr+1)%60
-    pc.r_wait=pc.r_wait-1
-    o.t = o.t-1
-    if ofc_state == "hit" then
-      o.FR = 1
-    end
-    if ofc_state == "run" then  
-      o.FR=(o.FR+1)%40
-    end
-  end --- end sprsheet03 ---
-
-  -- set runner anim sprites
   local function set_runSpr()   
-    if pc.indx == 1 then
+    if rt_pc.indx == 1 then
       jumpSpr_id  = 268
       slideSpr_id = 270
       hitSpr_id   = 416
-      sprSpd = pc.spdTbl[1]
       sprHit = .75
-    elseif pc.indx == 2 then
+    elseif rt_pc.indx == 2 then
       jumpSpr_id  = 300
       slideSpr_id = 302
       hitSpr_id   = 418
-      sprSpd = pc.spdTbl[2]
       sprHit = 1.5
-    elseif pc.indx == 3 then
+    elseif rt_pc.indx == 3 then
       jumpSpr_id  = 332
       slideSpr_id = 334
       hitSpr_id   = 420
-      sprSpd = pc.spdTbl[3]
       sprHit = 1.0
-    elseif pc.indx == 4 then
+    elseif rt_pc.indx == 4 then
       jumpSpr_id  = 364
       slideSpr_id = 366
       hitSpr_id   = 422
-      sprSpd = pc.spdTbl[4]
       sprHit = 1.25
     end
   end
+  local p = rt_pc -- ref to rt_pc for runner
   
+function rt_pcSpr()
+	if btnp(bttn.a,60,15) then 
+		rt_pc.indx=(rt_pc.indx>1) and rt_pc.indx-1 or 1
+		selected_pc=rt_pc.name[rt_pc.indx]
+		rt_pc.sprId=rt_pc.iniSpr[rt_pc.indx]
+	elseif btnp(bttn.s,60,15) then 
+		rt_pc.indx=(rt_pc.indx<=3)and rt_pc.indx+1 or 4
+		selected_pc=rt_pc.name[rt_pc.indx]
+		rt_pc.sprId=rt_pc.iniSpr[rt_pc.indx]
+	end
+end
+
+function rt_animate()
+	if rt_pc.wait == 0 then
+		rt_pc.changeFrame=true
+		rt_pc.CF=(rt_pc.CF==1) and 2 or 1
+		if rt_pc.isIdle then rt_pc.wait=30
+		elseif rt_pc.isRun then rt_pc.wait=15
+		end
+	end
+	if rt_pc.isIdle and rt_pc.changeFrame then
+		rt_pc.sprId=rt_pc.sprites[rt_pc.indx][rt_pc.CF]
+		rt_pc.changeFrame=false
+	elseif rt_pc.isRun and rt_pc.changeFrame then
+		rt_pc.sprId=rt_pc.sprites[rt_pc.indx][rt_pc.CF+4]
+		rt_pc.changeFrame=false
+	end
+end
+  
+function rt_draw()
+	rt_pcSpr()
+	rt_animate()
+	if (rt_pc.y >= 78) then
+		spr(445,ofc.x+6,101,5,2,1,0,1,1)--shadow officer
+		spr(445,rt_pc.x+8,rt_pc.y+24,5,2,1,0,1,1)--shadow rt_pc
+		spr(ofc.id+ofc.FR//20*2,ofc.x,78,0,2,ofc.flip,0,2,2)--oficcer
+		spr(rt_pc.sprId,rt_pc.x,rt_pc.y,rt_pc.CLRK,rt_pc.scale,rt_pc.flip,0,2,2)--rt_pc
+	else
+		spr(445,rt_pc.x+8,102,5,2,1,0,1,1)--shadow rt_pc
+		spr(445,ofc.x+6,101,5,2,1,0,1,1)--shadow officer
+		spr(rt_pc.sprId,rt_pc.x,rt_pc.y,rt_pc.CLRK,rt_pc.scale,rt_pc.flip,0,2,2)--rt_pc
+		spr(ofc.id+ofc.FR//20*2,ofc.x,78,0,2,ofc.flip,0,2,2)--officer
+	end
+end
+
+function print_debug(color)
+	print("Press A or S to change character",0,0,color or 3)	
+	print("Change frame: "..rt_pc.CF,0,7,color or 12)
+	print(", ID: "..rt_pc.sprId,83,7,color or 12)
+	print(", Character: "..selected_pc,126,7,color or 12)
+	print("UP to jump  DOWN to slide",0,14,color or 3)
+	print("player y: "..p.y,160,14,12)	
+end
+
+function runner_sprsheet03()
+ cls()
+ vbank(0)
+	map(mapX,17,30,17,0,0,-1)--bg
+	map(decor.x,decor.y,30,17,0,3,0,2)--decorations
+	vbank(1)
+	poke(0x03FF8,0)
+	map(mapX,17,30,17,0,0,-1)--bg
+	map(decor.x,decor.y,30,17,0,3,0,2)--decorations
+ rt_draw()
+
+	fr=(fr+1)%60
+	rt_pc.wait=rt_pc.wait-1
+	ofc.t = ofc.t-1
+	if ofc_state == "hit" then
+	  ofc.FR = 1
+	end
+	if ofc_state == "run" then  
+	  ofc.FR=(ofc.FR+1)%40
+ end
+end --- end sprsheet03 ---
+
   -- set runner direction
   run_dir = {right = false, left = false}
-  run_dir.right = true
+  run_dir.left = true
   -- defaults set to run left  
   new_ob_x = 0
-  obs_spd = 2  
+  obs_spd = 3  
   bst_spd = .5
   end_wallX = 0
 
@@ -185,11 +189,11 @@ function looping_runner_init()
     if btnp(0) and (grounded) then
       y_vel = -2.5
       mid_jump = true      
-      p.r_wait = 45
+	     p.wait = 45
       p.sprId = jumpSpr_id
     end
     if btnp(1) and (grounded) then
-      p.r_wait = 45
+      p.wait = 45
       p.sprId = slideSpr_id
       mid_slide = true
       tm.wt = 45        
@@ -226,10 +230,10 @@ function looping_runner_init()
   end
   
   function generate_obs()
-    if set_timer(1.5) then
+    if set_timer(1) then
       need_object = true
     end
-    if set_timer(12) then
+    if set_timer(10) then
       need_boost = true    
     end
     if (need_object) then
@@ -306,7 +310,7 @@ function looping_runner_init()
         if obs[i].active and
           (object_col(obs[i].xp, obs[i].yp)) then
            
-            p.r_wait = 30
+            p.wt = 30
             p.sprId = hitSpr_id
             show_setback = true
             cur_dist = cur_dist - sprHit
@@ -373,24 +377,24 @@ function looping_runner_init()
         if (run_dir.left) then 
           if ofc.x <= obs[i].xp+7 then
             ofc_state = "hit"
-            ofc.id = o.hitId
-            o.t = 8
+            ofc.id = ofc.hitId
+            ofc.t = 8
             table.remove(obs, i)
           end
         end
         if (run_dir.right) then 
           if ofc.x+31 >= obs[i].xp then
             ofc_state = "hit"
-            ofc.id = o.hitId
-            o.t = 8
+            ofc.id = ofc.hitId
+            ofc.t = 8
             table.remove(obs, i)
           end
         end
       end
     end
-    if o.t <= 0 then
+    if ofc.t <= 0 then
        ofc_state="run"
-       ofc.id=o.runId
+       ofc.id=ofc.runId
     end
   end 
   
@@ -434,48 +438,6 @@ function looping_runner_init()
     end
   end
   
-  function end_runner()
-      tm.gm_ct = 1
-      tm.intvl = 1
-      mp_spd = 0 -- stop scrolling
-      o.t=1
-      ofc.id=o.idleId
-      obs_spd = 0
-      bst_spd = 0
-      need_object = false
-      need_boost = false
-      table.remove(obs, 1)  -- clear obs
-      table.remove(boosts, 1)
-      grounded = false -- stop run_lvl_mv
-      if (gm_state.win) then 
-        rt_draw()
-        if (run_dir.left) then 
-          p.x = p.x - 1
-          if p.x <= 16 then
-            p.x = 16
-            p.isIdle = true
-            p.isRun=false
-            officer_result = "ran_away_success"
-            current_system = "continue_menu"
-          end
-        end
-        if (run_dir.right) then 
-          p.x = p.x + 1 
-          if p.x >= 192 then
-            p.x = 192
-            p.isIdle = true
-            p.isRun=false
-            officer_result = "ran_away_success"
-            current_system = "continue_menu"
-          end
-        end
-      end
-      if (gm_state.fail) then
-        officer_result = "ran_away_fail"
-        current_system = "continue_menu"      
-      end
-  end
-
   function update()
     tm.intvl = tm.intvl + 1
     tm.gm_ct = tm.gm_ct - 1
@@ -483,8 +445,8 @@ function looping_runner_init()
     p.y = p.y + y_vel
     -- "scroll" map
     mapX = mapX + mp_spd*sprSpd
-    decor.x = decor.x + mp_spd*sprSpd
-    -- win/lose conditions
+    decor.x = decor.x + (mp_spd*sprSpd)/2
+	   -- win/lose conditions
     if (cur_dist <= 0) then
       gm_state.fail = true
     end
@@ -496,7 +458,7 @@ function looping_runner_init()
        map(240-tm.gm_ct/60*16,68,240,136,0,0,0) 
       end
     end
-    if (tm.gm_ct/60 == 0) then -- 60 for testing
+    if (tm.gm_ct/60 <= 0) then -- 60 for testing
       gm_state.win = true
     end
   end
@@ -507,6 +469,51 @@ function looping_runner_init()
     display_col()
     print("Distance: "..cur_dist,6,128,12,false,1,true)
     print("Time: "..math.floor((tm.gm_ct/60)*10)/10,200,128,12,false,1,true)
+  end  
+  
+  function end_runner()
+    tm.gm_ct = 1
+    tm.intvl = 1
+    mp_spd = 0 -- stop scrolling
+    ofc.t=1
+    ofc.id=ofc.idleId
+    obs_spd = 0
+    bst_spd = 0
+    need_object = false
+    need_boost = false
+    table.remove(obs, 1)  -- clear obs
+    table.remove(boosts, 1)
+    grounded = false -- stop run_lvl_mv
+    if (gm_state.win) then 
+      rt_draw()
+      if (run_dir.left) then 
+        p.x = p.x - 1
+        map(0,85,240,136,0,0,0) -- end overlay
+        if p.x <= 0 then
+          p.isIdle = true
+          p.isRun=false
+          print("Win. Enter next system",120,75,2)
+          officer_result = "ran_away_success"
+          current_system = "continue_menu"
+        end
+      end
+      if (run_dir.right) then 
+        p.x = p.x + 1
+        map(28,102,240,136,224,0,0) -- overlay 
+        if p.x >= 240 then
+          p.isIdle = true
+          p.isRun=false
+          print("Win. Enter next system",120,75,2)
+          officer_result = "ran_away_success"
+          current_system = "continue_menu"
+        end
+      end
+    end
+    if (gm_state.fail) then
+      print("Fail. Enter next system",120,75,2)
+      officer_result = "ran_away_fail"
+      current_system = "continue_menu"      
+    end
   end
  
   function looping_runner_logic()
@@ -529,9 +536,9 @@ function looping_runner_init()
 end -- end looping_runner init
 
 looping_runner_init()
-  
+
 function looping_runner_loop()
- -- gsync(1|2|4|8|32|64|128, 2, false)  
+-- gsync(1|2|4|8|32|64|128, 2, false)
   looping_runner_logic()
 end
 
